b417f5d82a80186a915cb7990df1340d66d185a8
Dashon Howard <i@dashon.co>
2021-11-19 09:32:13 -0600

    update

--- src/components/sections/Experiences.js
+++ src/components/sections/Experiences.js
@@ -24,8 +24,8 @@ const educationData = [
     title: "ChopDawg (Contract)",
     years: "May 2020 - Jan 2021",
     content:
-      "Worked with a team of contractors on various projects, primarily using NodeJS or Laravel, ReactJS and SQL. Led the " +
-      "development of a blockchain application that utilized smart contracts interacting with smart home devices.",
+      "Worked with a team of contractors on various projects, primarily using, NodeJS, Laravel, ReactJS, and SQL. "+
+      "Led the development of a blockchain application that utilized Soldity contracts, web3JS, and Metamask interfacing with smart home devices. ",
   },
   {
     id: 4,
