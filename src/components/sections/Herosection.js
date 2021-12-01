2346649a997d8bbfbedc5d448aada1f157f50de1
Dashon Howard <i@dashon.co>
2021-12-01 00:26:19 -0600

    fix bug

--- src/components/sections/Herosection.js
+++ src/components/sections/Herosection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Typed from "react-typed";
 import { Link } from "react-scroll";
+import ReactGA from 'react-ga';
 
 function Herosection(props) {
   const { x, y } = props.position;
